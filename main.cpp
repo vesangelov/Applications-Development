7b400b9408841c6b1a2cec1aedf62b1223e7f101
vangelov <veselin_angelov_1992@abv.bg>
2023-01-27 10:22:51 +0200

    Correct setting on CMake to find SDL.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+
+#include <SDL.h>
+
+SDL_Surface* test = nullptr;
+
+int32_t main(int32_t [[__attribute_maybe_unused__]]argc, char* [[__attribute_maybe_unused__]]argv[]){
+
+    return EXIT_SUCCESS;
+}
+
+
+
+
+
